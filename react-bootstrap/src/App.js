61b1398ab4079dba785c42571374c277fac2ae77
MONLAU\martacalgua <martcalgua@campus.monlau.com>
2023-03-29 20:47:37 +0200

    react-bootstrap

---
+++ react-bootstrap/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+
+function ONGView() {
+  return (
+    <div>
+      <h1>MARTA ONG</h1>
+      <p>Información sobre la ONG...</p>
+      <Button variant="primary">Donar</Button>
+    </div>
+  );
+}
+export default ONGView;
